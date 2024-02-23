d1c2360f88bb7c9a0fc93e59b4bc2b7f683a766d
xiongjie.xue <xiongjie.xue@lkcoffee.com>
2024-02-23 18:52:40 +0800

    refactor: 💡 代码优化

--- library/src/main/ets/common/types/HtmlParser.d.ts
+++ library/src/main/ets/common/types/HtmlParser.d.ts
@@ -1,11 +1,21 @@
-export interface TagInfo {
-  tagName: string;
-  attrs: {
-    name: string;
-    value: string;
-    escaped: string
-  }[];
-  unary: boolean;
+export interface TextNode {
+  node: 'text';
+  text?: string;
+}
+
+export interface NodeInfo {
+  node: 'element' | 'text';
+  tag?: string;
+  $screen?: {
+    width: number;
+    height: number;
+  };
+  tagType?: 'block' | 'inline' | 'closeSelf';
+  styleObj?: Record<string, any>;
+  attr?: Record<string, any>;
+  classStr?: string;
+  text?: string;
+  nodes?: NodeInfo[];
 }
 
 export interface Handler {
